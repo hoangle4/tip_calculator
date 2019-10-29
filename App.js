bd7c6d1725f2070c5c8ed2437db7ca461b4b5f00
Hoang Le <hoang.le4@pcc.edu>
2019-10-29 10:01:19 -0700

    commit push

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { TOTAL_BILL, TOTAL_TIP, PARTIAL_BILL } from './types';
 export default function App() {
@@ -42,60 +42,62 @@ export default function App() {
 	const handleOnKeyPress = () => {};
 
 	return (
-		<Grid style={styles.grid}>
-			<Row style={styles.headerContainer} size={1}>
-				<Text style={styles.header}>Tips Split</Text>
-			</Row>
-			<Row size={3} style={styles.bodyContainer}>
-				<View style={{ marginBottom: '25px', marginTop: '25px' }}>
-					<Text style={{ color: '#fff', fontWeight: 'bold', padding: '5px' }}>Total Bill</Text>
-					<TextInput
-						keyboardType="numeric"
-						style={{ height: 40, backgroundColor: '#fff', fontWeight: 'bold', padding: '5px' }}
-						placeholder="Total bill here!"
-						onChangeText={(e) => handleInputOnChange({ type: TOTAL_BILL, payload: e })}
-						value={totalBill}
-						onKeyPress={handleOnKeyPress}
-					/>
-					<Text style={{ color: '#fff', fontSize: '10px' }}>Total bill without tips.</Text>
-				</View>
-				<View style={{ marginBottom: '25px' }}>
-					<Text style={{ color: '#fff', fontWeight: 'bold', padding: '5px' }}>Total Tips</Text>
-					<TextInput
-						keyboardType="numeric"
-						style={{ height: 40, backgroundColor: '#fff', fontWeight: 'bold', padding: '5px' }}
-						placeholder="Total bill here!"
-						onChangeText={(e) => handleInputOnChange({ type: TOTAL_TIP, payload: e })}
-						value={totalTip}
-					/>
-					<Text style={{ color: '#fff', fontSize: '10px' }}>Total tips without bill.</Text>
-				</View>
-				<View style={{ marginBottom: '25px' }}>
-					<Text style={{ color: '#fff', fontWeight: 'bold', padding: '5px' }}>Your portion of the bill</Text>
-					<TextInput
-						keyboardType="numeric"
-						style={{ height: 40, backgroundColor: '#fff', fontWeight: 'bold', padding: '5px' }}
-						placeholder="Total bill here!"
-						onChangeText={(e) => handleInputOnChange({ type: PARTIAL_BILL, payload: e })}
-						value={partialBill}
-					/>
-					<Text style={{ color: '#fff', fontSize: '10px' }}>your portion of the bill only.</Text>
-				</View>
+		<ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+			<Grid style={styles.grid}>
+				<Row style={styles.headerContainer} size={1}>
+					<Text style={styles.header}>Tips Split</Text>
+				</Row>
+				<Row size={3} style={styles.bodyContainer}>
+					<View style={{ marginBottom: 25, marginTop: 25 }}>
+						<Text style={{ color: '#fff', fontWeight: 'bold', padding: 5 }}>Total Bill</Text>
+						<TextInput
+							keyboardType="numeric"
+							style={{ height: 40, backgroundColor: '#fff', fontWeight: 'bold', padding: 5 }}
+							placeholder="Total bill here!"
+							onChangeText={(e) => handleInputOnChange({ type: TOTAL_BILL, payload: e })}
+							value={totalBill}
+							onKeyPress={handleOnKeyPress}
+						/>
+						<Text style={{ color: '#fff', fontSize: 10 }}>Total bill without tips.</Text>
+					</View>
+					<View style={{ marginBottom: 25 }}>
+						<Text style={{ color: '#fff', fontWeight: 'bold', padding: 5 }}>Total Tips</Text>
+						<TextInput
+							keyboardType="numeric"
+							style={{ height: 40, backgroundColor: '#fff', fontWeight: 'bold', padding: 5 }}
+							placeholder="Total bill here!"
+							onChangeText={(e) => handleInputOnChange({ type: TOTAL_TIP, payload: e })}
+							value={totalTip}
+						/>
+						<Text style={{ color: '#fff', fontSize: 10 }}>Total tips without bill.</Text>
+					</View>
+					<View style={{ marginBottom: 25 }}>
+						<Text style={{ color: '#fff', fontWeight: 'bold', padding: 5 }}>Your portion of the bill</Text>
+						<TextInput
+							keyboardType="numeric"
+							style={{ height: 40, backgroundColor: '#fff', fontWeight: 'bold', padding: 5 }}
+							placeholder="Total bill here!"
+							onChangeText={(e) => handleInputOnChange({ type: PARTIAL_BILL, payload: e })}
+							value={partialBill}
+						/>
+						<Text style={{ color: '#fff', fontSize: 10 }}>your portion of the bill only.</Text>
+					</View>
 
-				<View style={{ marginBottom: '25px' }}>
-					<Text style={{ color: '#fff', fontWeight: 'bold', padding: '5px' }}>
-						Tips Percentage (%) : {(tipPercentage !== NaN && tipPercentage) || 0} %
-					</Text>
-					<Text style={{ color: '#fff', fontWeight: 'bold', padding: '5px' }}>
-						Your Tips ($) :{' '}
-						<Text style={{ color: '#39ff14', fontWeight: 'bold', padding: '5px' }}>
-							{' '}
-							${(tipPortion !== NaN && tipPortion) || 0}
+					<View style={{ marginBottom: 25 }}>
+						<Text style={{ color: '#fff', fontWeight: 'bold', padding: 5 }}>
+							Tips Percentage (%) : {(tipPercentage !== NaN && tipPercentage) || 0} %
 						</Text>
-					</Text>
-				</View>
-			</Row>
-		</Grid>
+						<Text style={{ color: '#fff', fontWeight: 'bold', padding: 5 }}>
+							Your Tips ($) :{' '}
+							<Text style={{ color: '#39ff14', fontWeight: 'bold', padding: 5, fontSize: 20 }}>
+								{' '}
+								${(tipPortion !== NaN && tipPortion) || 0}
+							</Text>
+						</Text>
+					</View>
+				</Row>
+			</Grid>
+		</ScrollView>
 	);
 }
 
@@ -105,7 +107,7 @@ const styles = StyleSheet.create({
 	},
 	header: {
 		color: 'darkblue',
-		fontSize: '25px',
+		fontSize: 25,
 		fontWeight: 'bold'
 	},
 	headerContainer: {
@@ -117,6 +119,8 @@ const styles = StyleSheet.create({
 	bodyContainer: {
 		backgroundColor: 'darkblue',
 		flexDirection: 'column',
-		alignItems: 'center'
+		paddingLeft: 35,
+		paddingRight: 35,
+		alignItems: 'stretch'
 	}
 });
